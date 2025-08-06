ab3698cf7e5c6af449ade097dcb7f09415b940f9
Sujal Manvar <sujalmanvar2007@gmail.com>
2025-08-06 18:37:29 +0530

    secound

--- app/src/main/java/com/example0009/pr_5/MainActivity.java
+++ app/src/main/java/com/example0009/pr_5/MainActivity.java
@@ -1,13 +1,8 @@
 package com.example0009.pr_5;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,10 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
